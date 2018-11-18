f1022e523f8dc694c8c1c7835435563e26df58ec
Philip <hohohsh@icloud.com>
2018-11-18 15:59:51 -0500

    Warbler AuthForm Completed

--- warbler-client/src/containers/App.js
+++ warbler-client/src/containers/App.js
@@ -3,14 +3,16 @@ import { Provider } from "react-redux";
 import { configureStore } from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Navbar";
+import Main from './Main';
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div class="onboarding">
+      <div className="onboarding">
         <Navbar/>
+        <Main />
       </div>
     </Router>
   </Provider>
